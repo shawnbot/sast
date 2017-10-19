c761637b25f5678942fe5ee3f05ad6f4262fb300
Shawn Allen <shawn.allen@github.com>
2017-10-19 01:38:20 -0700

    fix bug with parseFile() 'auto' syntax

--- src/io.js
+++ src/io.js
@@ -7,7 +7,8 @@ const parseFile = (filename, parseOptions={}, readOptions='utf8') => {
   let {syntax} = parseOptions
   if (!syntax || syntax === SYNTAX_AUTO) {
     if (filename.indexOf('.') > -1) {
-      syntax = filename.split('.').pop()
+      parseOptions.syntax = filename.split('.').pop()
+      console.warn('detected auto syntax:', syntax)
     } else {
       throw new Error(
         `"auto" syntax requires a filename with an extension; got "${filename}"`
