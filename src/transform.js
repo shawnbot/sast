83bf3f751c65ec4344f0b508075d43ea3e95f057
Shawn Allen <shawn.allen@github.com>
2017-10-19 01:39:09 -0700

    allow parentheses with "," or ":" operators

--- src/transform.js
+++ src/transform.js
@@ -9,15 +9,11 @@ const isOperator = node => is([COLON, COMMA], node)
 
 const parens = node => {
   const op = node.children.filter(isOperator)[0]
-  invariant(
-    op,
-    `Expected at least one "operator" child; got: ${
-      node.children.map(c => c.type).join(', ')
-    }`
-  )
-  switch (op.value) {
-    case COLON.value: return mapify(node)
-    case COMMA.value: return listify(node)
+  if (op) {
+    switch (op.value) {
+      case COLON.value: return mapify(node)
+      case COMMA.value: return listify(node)
+    }
   }
   return node
 }
